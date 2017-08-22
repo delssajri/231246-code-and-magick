6ffa6c1b8b9d28da5a812433440ab7f164325586
Tanya Drozdova <delssajri@gmail.com>
2017-08-23 01:44:19 +0300

    fix travis

--- js/stat.js
+++ js/stat.js
@@ -3,11 +3,11 @@
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.strokeRect(110, 20, 420, 270);
-  ctx.fillRect(110, 20, 420, 270);  // Создадим прямоугольник черного цвета
+  ctx.fillRect(110, 20, 420, 270); // Создадим прямоугольник черного цвета
   ctx.fillStyle = 'rgb(255, 255, 255)';
   ctx.strokeRect(100, 10, 420, 270);
   ctx.fillRect(100, 10, 420, 270); // Создадим прямоугольник белого цвета
-  ctx.fillStyle = '#000'; 
+  ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
@@ -30,23 +30,21 @@ window.renderStatistics = function (ctx, names, times) {
     }
   }
 
-  var step = barHeight/max;
+  var step = barHeight / max;
 
-  for (var i = 0; i < times.length; i++) {
+  for (var j = 0; j < times.length; j++) {
     var rand = Math.random();
-    ctx.fillStyle = names[i] == 'Вы' ? colorme : 'rgba(38, 31, 240,' + rand +')';
+    ctx.fillStyle = names[j] === 'Вы' ? colorme : 'rgba(38, 31, 240,' + rand + ')';
 
-    if (i == maxIndex) {
+    if (j === maxIndex) {
       initialYNew = initialY;
-    }
-    else {
-      initialYNew = initialY + barHeight - times[i] * step;
+    } else {
+      initialYNew = initialY + barHeight - times[j] * step;
     }
 
-    var widthBase = i * (indent + histogramWidth);
-    ctx.fillText(Math.round(times[i]), initialX + widthBase, initialYNew - 10);
-    ctx.fillRect(initialX + widthBase, initialYNew, histogramWidth, times[i] * step); // Строим гисторгамму
-    ctx.fillText(names[i], initialX + widthBase, initialYNew + times[i] * step + 20);
+    var widthBase = j * (indent + histogramWidth);
+    ctx.fillText(Math.round(times[j]), initialX + widthBase, initialYNew - 10);
+    ctx.fillRect(initialX + widthBase, initialYNew, histogramWidth, times[j] * step); // Строим гисторгамму
+    ctx.fillText(names[j], initialX + widthBase, initialYNew + times[j] * step + 20);
   }
-
-}
+};
